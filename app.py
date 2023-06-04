41eb88f7af4a40c0131800afbc80d8c9412b3e19
Thoriq-ha <ithoriq0@gmail.com>
2023-06-04 11:35:48 +0700

    logger

--- app.py
+++ app.py
@@ -2,12 +2,18 @@
 from pathlib import Path
 import PIL
 
+
 # External packages
 import streamlit as st
 
 # Local Modules
 import settings
 import helper
+
+import os
+
+os.environ['KMP_DUPLICATE_LIB_OK']='True'
+
 
 # Setting page layout
 st.set_page_config(
@@ -25,7 +31,7 @@ st.sidebar.header("ML Model Config")
 
 # Model Options
 model_type = st.sidebar.radio(
-    "Select Task", ['Detection', 'Segmentation'])
+    "Select Task", ['Detection'])
 
 confidence = float(st.sidebar.slider(
     "Select Model Confidence", 25, 100, 40)) / 100
@@ -33,8 +39,7 @@ confidence = float(st.sidebar.slider(
 # Selecting Detection Or Segmentation
 if model_type == 'Detection':
     model_path = Path(settings.DETECTION_MODEL)
-elif model_type == 'Segmentation':
-    model_path = Path(settings.SEGMENTATION_MODEL)
+
 
 # Load Pre-trained ML Model
 try:
--- helper.py
+++ helper.py
@@ -2,8 +2,12 @@ from ultralytics import YOLO
 import streamlit as st
 import cv2
 import pafy
-
+import geocoder
+from fpdf import FPDF
+import base64
+import pandas as pd
 import settings
+from datetime import datetime
 
 
 def load_model(model_path):
@@ -21,15 +25,17 @@ def load_model(model_path):
 
 
 def display_tracker_options():
-    display_tracker = st.radio("Display Tracker", ('Yes', 'No'))
-    is_display_tracker = True if display_tracker == 'Yes' else False
+    # display_tracker = st.radio("Display Tracker", ('Yes', 'No'))
+    # is_display_tracker = True if display_tracker == 'Yes' else False
+    is_display_tracker = True
     if is_display_tracker:
-        tracker_type = st.radio("Tracker", ("bytetrack.yaml", "botsort.yaml"))
+        tracker_type = "bytetrack.yaml"
+        # tracker_type = st.radio("Tracker", ("bytetrack.yaml", "botsort.yaml"))
         return is_display_tracker, tracker_type
     return is_display_tracker, None
 
 
-def _display_detected_frames(conf, model, st_frame, image, is_display_tracking=None, tracker=None):
+def _display_detected_frames(conf, model, st_frame, image, is_display_tracking=None, tracker=None, df=None):
     """
     Display the detected objects on a video frame using the YOLOv8 model.
 
@@ -54,13 +60,33 @@ def _display_detected_frames(conf, model, st_frame, image, is_display_tracking=N
         # Predict the objects in the image using the YOLOv8 model
         res = model.predict(image, conf=conf)
 
+    names = model.names
     # # Plot the detected objects on the video frame
     res_plotted = res[0].plot()
+
+    g = geocoder.ip('me')
+    for r in res:
+        for c in r.boxes.cls:
+            if(names[int(c)] != 'KAClipLengkap'):
+                label = names[int(c)]
+                location = g.latlng
+                img = res_plotted
+                
+                filenameimg = datetime.now().strftime('%Y-%m-%d--%H-%M-%S')
+                cv2.imwrite(f'./logger/image_logger/{filenameimg}.jpg', img)   
+                
+                df.loc[len(df.index)] = [label, location, f'{filenameimg}.jpg'] 
+                
+                st.write(label, location)
+                st.image(img)
+
+    filename = datetime.now().strftime('%Y-%m-%d--%H-%M')
+    df.to_csv(f'./logger/data_logger/{filename}.csv')
     st_frame.image(res_plotted,
-                   caption='Detected Video',
-                   channels="BGR",
-                   use_column_width=True
-                   )
+                caption='Detected Video',
+                channels="BGR",
+                use_column_width=True
+                )
 
 
 def play_youtube_video(conf, model):
@@ -177,6 +203,10 @@ def play_webcam(conf, model):
         except Exception as e:
             st.sidebar.error("Error loading video: " + str(e))
 
+def create_download_link(val, filename):
+                        b64 = base64.b64encode(val)  # val looks like b'...'
+                        return f'<a href="data:application/octet-stream;base64,{b64.decode()}" download="{filename}.pdf">Download file</a>'
+
 
 def play_stored_video(conf, model):
     """
@@ -192,31 +222,54 @@ def play_stored_video(conf, model):
     Raises:
         None
     """
-    source_vid = st.sidebar.selectbox(
-        "Choose a video...", settings.VIDEOS_DICT.keys())
 
-    is_display_tracker, tracker = display_tracker_options()
+    source_vid = st.sidebar.file_uploader(
+        "Choose an image...", type=("mp4", "mkv"))
+    
+    file_path = ''
+    if source_vid is not None:
+    # source_vid = st.sidebar.selectbox(
+    #     "Choose a video...", settings.VIDEOS_DICT.keys())
 
-    with open(settings.VIDEOS_DICT.get(source_vid), 'rb') as video_file:
-        video_bytes = video_file.read()
-    if video_bytes:
-        st.video(video_bytes)
+        # file_details = {"FileName": source_vid.name, "FileType": source_vid.type}
+        file_path = f"./videos/{source_vid.name}"
+        try:
+            with open(file_path, "wb") as f:
+                f.write(source_vid.getbuffer())
+            st.write("File berhasil diunggah.")
+        except:
+            st.write(" ")
+    
+        # st.text(file_path)
+        is_display_tracker, tracker = display_tracker_options()
+            
+        with open(file_path, 'rb') as video_file:
+            video_bytes = video_file.read()
+        if video_bytes:
+            st.video(video_bytes)
 
     if st.sidebar.button('Detect Video Objects'):
         try:
-            vid_cap = cv2.VideoCapture(
-                str(settings.VIDEOS_DICT.get(source_vid)))
+            vid_cap = cv2.VideoCapture(file_path)
             st_frame = st.empty()
+            dict = {
+                'Label':[],
+                'Location':[],
+                'gambar':[]
+            }
+
+            df = pd.DataFrame(dict)
             while (vid_cap.isOpened()):
                 success, image = vid_cap.read()
                 if success:
                     _display_detected_frames(conf,
-                                             model,
-                                             st_frame,
-                                             image,
-                                             is_display_tracker,
-                                             tracker
-                                             )
+                                            model,
+                                            st_frame,
+                                            image,
+                                            is_display_tracker,
+                                            tracker, df
+                                            )
+                    
                 else:
                     vid_cap.release()
                     break
